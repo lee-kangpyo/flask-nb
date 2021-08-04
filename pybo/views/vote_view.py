1a3c6e956ef993afe3ed0d348dfe947c080ed530
MSI02 <darkzhon@hanmail.net>
2021-08-04 16:56:21 +0900

    댓글 추천 기능 추가

--- pybo/views/vote_view.py
+++ pybo/views/vote_view.py
@@ -5,9 +5,11 @@ from werkzeug.utils import redirect
 
 from pybo import db
 from pybo.models import Question
+from pybo.models import Answer
 from ..forms import QuestionForm, AnswerForm
 
 from pybo.views.auth_views import login_required
+
 
 bp = Blueprint('vote', __name__, url_prefix='/vote')
 
@@ -15,6 +17,7 @@ bp = Blueprint('vote', __name__, url_prefix='/vote')
 @bp.route("/question/<int:question_id>")
 @login_required
 def question(question_id):
+
     _question = Question.query.get_or_404(question_id)
     if g.user == _question.user:
         flash('본인이 작성한 글은 추천할수 없습니다')
@@ -22,3 +25,16 @@ def question(question_id):
         _question.voter.append(g.user)
         db.session.commit()
     return redirect(url_for('question.detail', question_id=question_id))
+
+
+@bp.route("/answer/<int:answer_id>/")
+@login_required
+def answer(answer_id):
+    #import pdb; pdb.set_trace()
+    _answer = Answer.query.get_or_404(answer_id)
+    if g.user == _answer.user:
+        flash("본인이 작성한 글은 추천할수 없습니다.")
+    else:
+        _answer.voter.append(g.user)
+        db.session.commit()
+    return redirect(url_for('question.detail', question_id=_answer.question.id))
